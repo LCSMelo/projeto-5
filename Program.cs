2080417ad5d3142fa78ec091ed65237d42e9628c
Lucas Leonardo de Jesus de Melo <39694603870@jpmorgan.sp>
2017-10-26 18:14:01 -0200

    new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projeto_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
